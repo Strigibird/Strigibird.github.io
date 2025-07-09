8ce25714a4005583119d2ffb1a1ff552034b45c7
Strigibird <83137001+Strigibird@users.noreply.github.com>
2025-07-10 01:22:43 +0200

    Objects by script!
    The confirm button now creates copies of a button with their own internal code

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 var message = "hello world";
-document.getElementById("Confirm").onclick=function() {
+document.getElementById("Csets").onchange=function() {
     alert("Yoo what's up my "+document.getElementById("Csets").value+" men");
 }
+i=1;
+document.getElementById("Confirm").onclick=function() {
+    newContent = document.createElement("br");
+    newDiv.appendChild(newContent);
+    newContent = document.createElement("button");
+    newContent.textContent="Copy";
+    newContent.id="number "+i;
+    i+=1;
+    newContent.addEventListener("click", function() {
+        alert(event.target.id);
+    })
+    newDiv.appendChild(newContent);
+}
+newDiv = document.createElement("div");
+newContent = document.createTextNode("Hiiii :3");
+currentDiv = document.getElementById("End");
+document.body.insertBefore(newDiv,currentDiv);
+newDiv.appendChild(newContent);
