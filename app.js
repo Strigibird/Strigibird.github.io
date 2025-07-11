c0334d953d0923219d76bf640512239d58068bd0
Strigibird <83137001+Strigibird@users.noreply.github.com>
2025-07-11 02:52:18 +0200

    Command sets
    Now you can dynamically change the amount of command sets while building your strategy. Even while deleting them as long as you are on the page the website will remember every div created

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-var message = "hello world";
+fullDiv = [];
+
+//You get one by default
+AddCommandSet();
+
+document.getElementById("Commandsets").appendChild(fullDiv[0]);
+
 document.getElementById("Csets").onchange=function() {
-    alert("Yoo what's up my "+document.getElementById("Csets").value+" men");
+    //Add new commandsets until the amount matches
+    while (event.target.value>fullDiv.length) {
+        AddCommandSet();
+    }
+    document.getElementById("Commandsets").innerHTML="";
+    for (i=0;i<event.target.value;i+=1) {
+        document.getElementById("Commandsets").appendChild(fullDiv[i]);
+    }
 }
-i=1;
-document.getElementById("Confirm").onclick=function() {
-    newContent = document.createElement("br");
-    newDiv.appendChild(newContent);
-    newContent = document.createElement("button");
-    newContent.textContent="Copy";
-    newContent.id="number "+i;
-    i+=1;
-    newContent.addEventListener("click", function() {
-        alert(event.target.id);
-    })
-    newDiv.appendChild(newContent);
+
+function AddCommandSet() {
+        //Makes smaller div to avoid fucking up the count.
+        smallDiv = document.createElement("div");
+        // breaks to make it look better
+        smallDiv.appendChild(document.createElement("br"));
+        // The question
+        newContent = document.createElement("h3");
+        newContent.textContent="How many commands are in command set "+(fullDiv.length+1)+"?"
+        smallDiv.appendChild(newContent);
+        // The input
+        newContent = document.createElement("input");
+        newContent.type="number";
+        newContent.value=1;
+        newContent.min=1;
+        //Makes it follow any function on change
+        newContent.addEventListener("change", function() {
+            alert(event.target.value);
+        })
+        smallDiv.appendChild(newContent);
+        // Add smallDiv to fullDiv so every commandset is just one element
+        fullDiv.push(smallDiv);
 }
-newDiv = document.createElement("div");
-newContent = document.createTextNode("Hiiii :3");
-currentDiv = document.getElementById("End");
-document.body.insertBefore(newDiv,currentDiv);
-newDiv.appendChild(newContent);
+
+//Test button
+document.getElementById("Reveal").onclick=function() {
+    alert(fullDiv.length)
+}
