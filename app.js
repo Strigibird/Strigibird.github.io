b0e7aef117a08bf9cce86649af6c61fad3ad53aa
Strigibird <83137001+Strigibird@users.noreply.github.com>
2025-07-21 02:01:20 +0200

    ELSE commands
    Added the default commands that every commandset has. Will fix the dropdowns after

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 fullDiv = [];
 
+specDiv=[[]];
+
 //You get one by default
 AddCommandSet();
 
@@ -12,6 +14,7 @@ document.getElementById("Csets").onchange=function() {
     }
     document.getElementById("Commandsets").innerHTML="";
     for (i=0;i<event.target.value;i+=1) {
+        //Updating command sets visibility.
         document.getElementById("Commandsets").appendChild(fullDiv[i]);
     }
 }
@@ -35,11 +38,51 @@ function AddCommandSet() {
             alert(event.target.value);
         })
         smallDiv.appendChild(newContent);
+
+
+        //Add div containing all commands to every commandset
+        newContent=document.createElement("div");
+
+        //Final command header
+        newerContent=document.createElement("h4");
+        newerContent.textContent="Command ELSE";
+        newContent.appendChild(newerContent);
+
+        //Final command text 1
+        newerContent=document.createElement("p");
+        newerContent.textContent="Strategy: All else";
+        newContent.appendChild(newerContent);
+        
+        //Final command text 2
+        newerContent=document.createElement("p");
+        newerContent.textContent="Response: ";
+        newContent.appendChild(newerContent);
+        //Final command input 2
+        newerContent=document.createElement("input");
+        newContent.appendChild(newerContent);
+        // breaks to make it look better
+        newContent.appendChild(document.createElement("br"));
+
+        //Final command text 3
+        newerContent=document.createElement("p");
+        newerContent.textContent="What command set will you go to?";
+        newContent.appendChild(newerContent);
+        //Final command input 3
+        newerContent=document.createElement("select");
+        newContent.appendChild(newerContent);
+
+        //Add this div to smallDiv by default
+        smallDiv.appendChild(newContent);
+
+        //Add this div to specDiv too
+        specDiv.push([]);
+        specDiv[specDiv.length-1].push(newContent);
+
         // Add smallDiv to fullDiv so every commandset is just one element
         fullDiv.push(smallDiv);
 }
 
 //Test button
 document.getElementById("Reveal").onclick=function() {
-    alert(fullDiv.length)
+    alert(specDiv);
 }
